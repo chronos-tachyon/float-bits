d56af434ed8d2bc3918d8d5f9ac92302a86fd96c
Donald King <chronos@chronos-tachyon.net>
2025-09-01 10:09:41 -0700

    Add missing fields to Cargo.toml and re-word the README.

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,10 @@
-//! Hashable, totally-ordered floats stored as bits.
+//! Floats stored as raw bits, making them hashable and totally ordered.
 //!
-//! This crate provides types that represent IEEE 754 binary floating point numbers, using unsigned
-//! integers to store the raw bits.
+//! The types in this crate represent IEEE 754 binary floating point numbers, using unsigned
+//! integers to store the raw bits of the floats.  Some of these types represent float formats not
+//! supported by Rust, or only if specific Rust features are available and enabled.  A limited
+//! suite of operations are available that operate directly on the raw bit representation,
+//! bypassing the need for Rust support.
 //!
 //! # Example
 //!
